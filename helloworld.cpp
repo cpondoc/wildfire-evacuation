f21a77b72740286960560f4ab13a25cf687d3a16
cpondoc <chris.pondoc@comcast.net>
2023-02-19 16:50:38 -0800

    initialize image of a gridworld

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,12 +4,33 @@
 
 using namespace std;
 
+/*
+Function to help draw grid on the screen
+*/
+void drawGridworld() {
+    cout << "Picture of Grid World" << endl;
+
+    // Simple CLI grid
+    int gridDim = 10;
+    for (int i = 0; i < 1 + gridDim; i++) {
+        for (int j = 0; j < gridDim; j++) {
+            if (i == 0) {
+                cout << "__";
+            } else {
+                cout << "|_";
+            }
+        }
+        if (i > 0) {
+            cout << "|" << endl;
+        } else {
+            cout << "_" << endl;
+        }
+    }
+}
+
 int main()
 {
-    int x = 6;
-    for (int i = 0; i < x; i++) {
-        cout << i << endl;
-    }
-    cout << "Hello World!" << endl;
+    // Initial function to draw grid world
+    drawGridworld();
     return 0;
 }
