b5d6ec22a106ccdf5d26f48b0e4598f3173036ba
cpondoc <chris.pondoc@comcast.net>
2023-02-19 16:36:01 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CS 238 Final Project
+By: Chris Pondoc, Joey Guman, and Joey O'Brien
+
+## Project Description
+Looking at wildfire evacuation
---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int x = 6;
+    for (int i = 0; i < x; i++) {
+        cout << i << endl;
+    }
+    cout << "Hello World!" << endl;
+    return 0;
+}
