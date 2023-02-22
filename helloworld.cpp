4b1e03e41199602f516574c4c16ad374d58070a7
Chris Pondoc <chris.pondoc@comcast.net>
2023-02-21 16:03:56 -0800

    Created some helper functions and finished up most work with respect to wildfire spread

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <stdlib.h>
 #include <string>
@@ -10,11 +11,10 @@ using namespace std;
 /*
 Function to help draw grid on the screen
 */
-void drawGridworld() {
+void drawGridworld(int gridDim) {
     cout << "Picture of Grid World" << endl;
 
     // Simple CLI grid
-    int gridDim = 10;
     for (int i = 0; i < 1 + gridDim; i++) {
         for (int j = 0; j < gridDim; j++) {
             if (i == 0) {
@@ -31,31 +31,65 @@ void drawGridworld() {
     }
 }
 
+/*
+Function to help with boolean variable signaling of cell burning
+*/
+bool isCellBurning() {
+	return ((rand() % 10 + 1) % 2 == 0);
+}
+
+/*
+Function to calculte Euclidean distance between two points
+*/
+float calculateDistance(int xOne, int yOne, int xTwo, int yTwo) {
+	return sqrt(pow(xOne - xTwo, 2) + pow((yOne - yTwo), 2));
+}
+
 int main()
-{
-    // Initial function to draw grid world
-    //drawGridworld();
-	
-	// Initialize random seed
+{	
+	// Initialize random seed and grid dimension
 	srand (time(NULL));
+	int gridDim = 5;
 	
 	/*
-	Initial stochastic model for burning of the fire
+	Initial stochastic model for fuel of a cell
 	*/
+	cout << "Modeling of amount of fuel!" << endl << endl;
 	    
     // Defining burning rate
     int burningRate = 5;
     int totalFuel = 50;
     
+    // Changing of the amout of fuel
     for (int i = 0; i < 10; i++) {
-    	// Generate random number and decide to turn on or off with even probability
-		int burningBool = rand() % 10 + 1;
-    	if (burningBool % 2 == 0) {
+		// Check if cell is burning and reduce accordingly
+    	if (isCellBurning()) {
     		totalFuel = max(0, totalFuel - burningRate);
 		}
 		
 		cout << "Remaining fuel left: " << totalFuel << endl;
 	}
+	
+	/*
+	Initial stochastic model for movement of the fire
+	*/
+	cout << endl << "Modeling spread of the fire!" << endl << endl;
+	
+	// Generate random points on the plane
+	int x = rand() % gridDim;
+	int y = rand() % gridDim;
+	
+	// Investigate all existing points around it
+	int pointsAround[4][2] = {{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}};
+	for (int  i = 0; i < 4; i++) {
+		// Check if added point is still within bounds
+		if ((0 <= pointsAround[i][0] && pointsAround[i][0] < gridDim) && (0 <= pointsAround[i][1] && pointsAround[i][1] < gridDim)) {
+			cout << "ioj";
+		}
+	}
+	
+	// Initial function to draw grid world
+    // drawGridworld(5);
     
     return 0;
 }
