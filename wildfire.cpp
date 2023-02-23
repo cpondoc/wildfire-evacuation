0d30ce5468cf33b6e7889e04dd168e7f9457ac2a
JosephGuman <joeytg@stanford.edu>
2023-02-22 19:05:27 -0800

    Added sampleNextStates to efficiently sample multiple future states

--- wildfire.cpp
+++ wildfire.cpp
@@ -13,6 +13,9 @@ Initial start with modeling wildfire spread!
 
 using namespace std;
 
+random_device rd;
+mt19937 gen(rd());
+
 struct LandCell{
 	bool fire = false;
 	double fuel;
@@ -90,8 +93,8 @@ void runFuelDepletion(vector<vector<LandCell> >& state){
 //Presumes fire can only spread across five cells
 vector<vector<LandCell> > sampleNextState(vector<vector<LandCell> >& state, double distanceConstant){
 
-	random_device rd;
-	mt19937 gen(rd());
+	//random_device rd;
+	//mt19937 gen(rd());
 	vector<vector<LandCell> > newState = state;
 	int observationDistance = 2;
 	//Goes through each point
@@ -118,6 +121,60 @@ vector<vector<LandCell> > sampleNextState(vector<vector<LandCell> >& state, doub
 	return newState;
 }
 
+//Presumes fire can only spread across five cells
+vector<vector<vector<LandCell> > > sampleNextStates(vector<vector<LandCell> >& state, double distanceConstant, int total){
+	vector<vector<vector<LandCell> > > futureStates;
+
+	vector<vector<double> > probabilities(state.size(), vector<double>(state[0].size()));
+	int observationDistance = 2;
+	//Goes through each point to build probability of there being a fire
+	for(int i = 0; i < state.size(); i++){
+		for(int j = 0; j < state[0].size(); j++){
+
+			if(!state[i][j].fire && state[i][j].fuel > 0){
+				double prob = 1;
+
+				for(int nI = max(0, i - observationDistance); nI < min(int(state.size()), i + observationDistance); nI++){
+					for(int nJ = max(0, j - observationDistance); nJ < min(int(state.size()), j + observationDistance); nJ++){
+						if(i != nI && j != nJ){
+							prob *= 1 - (pow(1.0 / calculateDistance(i, j, nI, nJ) / distanceConstant, 2) * (state[nI][nJ].fire ? 1 : 0));
+						}
+					}
+				}
+				prob = 1 - prob;
+				probabilities[i][j] = prob;
+			}
+			else
+				probabilities[i][j] = 0;
+		}
+	}
+
+	//Builds a future sample for each 
+	for(int t = 0; t < total; t++){
+
+		futureStates.push_back(vector<vector<LandCell> >(state.size(), vector<LandCell>(state[0].size())));
+
+		for(int i = 0; i < state.size(); i++){
+			for(int j = 0; j < state[0].size(); j++){
+				futureStates[futureStates.size() - 1][i][j] = state[i][j];
+				if(!state[i][j].fire){
+					bernoulli_distribution b(probabilities[i][j]);
+					futureStates[futureStates.size() - 1][i][j].fire = b(gen);
+				}
+				
+			}
+		}
+	}
+
+	for(int i = 0 ; i < total; i++){
+		printData(futureStates[i]);
+		cout << endl;
+	}
+
+	exit(0);
+
+	return futureStates;
+}
 void runSimulation(int gridDim, double distanceConstant, int burnRate){
 	random_device rd;
     mt19937 gen(rd());
@@ -138,6 +195,8 @@ void runSimulation(int gridDim, double distanceConstant, int burnRate){
 		}
 	}
 
+	sampleNextStates(state, distanceConstant, 10);
+
 	//Runs for x timesteps
 	for(int i = 0; i < 30; i++){
 		runFuelDepletion(state);
@@ -146,9 +205,6 @@ void runSimulation(int gridDim, double distanceConstant, int burnRate){
 			printData(state);
 			cout << endl;
 		}
-			
-
-			
 	}
 
 	
