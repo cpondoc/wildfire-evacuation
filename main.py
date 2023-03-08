6ae06b117330d98118467848a2398ba7069f9120
cpondoc <chris.pondoc@comcast.net>
2023-03-07 21:52:00 -0800

    push starter openai gym

---
+++ main.py
@@ -0,0 +1,18 @@
+import gym
+from gym import spaces
+
+class WildfireEnv(gym.Env):
+    def __init__(self):
+        # Observation space:
+        # Currently just the array of all the vector areas
+        # In the future: a tensor to pass into a stable baselines learning algorithm
+        self.observation_space = spaces.Dict(
+            {
+                "agents": spaces.Discrete(4),
+                "targets": spaces.Discrete(3)
+            }
+        )
+
+        # Action space eventually has to be:
+        # (# of populated areas, # of discrete actions to take)
+        self.action_space = spaces.Tuple(spaces.Discrete(4), spaces.Discrete(2))
